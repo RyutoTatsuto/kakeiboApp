1d5a0f6e94912641db08b5947d568df0df1560ef
RyutoSuzuki <no.asics@gmail.com>
2022-10-02 07:29:57 +0000

    住民テーブルのadminの表示項目を設定

--- cms/admin.py
+++ cms/admin.py
@@ -2,3 +2,16 @@ from django.contrib import admin
 from cms.models import PurchaseHistories, Residents, MajorCategories, PaymentMethods
 
 ''' DBのレコードの項目が詳細に見られるようにカスタマイズ '''
+
+
+class ResidentsAdmin(admin.ModelAdmin):
+    ''' 
+    住民テーブル
+    '''
+    # adminに表示する項目
+    list_display = ('resident_id', 'first_name', 'given_name',)
+    # クリックできる項目
+    list_display_links = ('resident_id',)
+
+
+admin.site.register(Residents, ResidentsAdmin)
