83281f6e12a77fc6ba0c0a2589ecdff180d5dde5
RyutoSuzuki <no.asics@gmail.com>
2022-10-02 07:21:19 +0000

    adminで既存のレコード名のみが見える状態になっていたが、詳細な項目が見れるように既存のコードを削除

--- cms/admin.py
+++ cms/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
 from cms.models import PurchaseHistories, Residents, MajorCategories, PaymentMethods
-
-admin.site.register(PurchaseHistories)
-admin.site.register(Residents)
-admin.site.register(MajorCategories)
-admin.site.register(PaymentMethods)
