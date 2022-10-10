7a3b78dcf6deb7028df438430d56b818d392cfca
RyutoSuzuki <no.asics@gmail.com>
2022-10-10 05:08:23 +0000

    購入履歴の一覧を全てid順に返す関数を作成

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from cms.models import PurchaseHistories
+
+
+def purchase_histories_list(request):
+    """ 購入履歴の一覧を表示する """
+    purchase_histories_list = (PurchaseHistories
+                               .objects
+                               .all()
+                               .order_by('purchase_history_id'))
+    
+    return render(request,
+                  'cms/',
+                  {'purchase_histories_list': purchase_histories_list})
