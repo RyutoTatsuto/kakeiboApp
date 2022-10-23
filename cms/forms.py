7b66c2df728aa74d013765c16494f2a7d06ae7bc
RyutoSuzuki <no.asics@gmail.com>
2022-10-23 00:04:46 +0000

    PurchaseHistoriesにアイテムを追加・修正できるフォームクラスを作成

---
+++ cms/forms.py
@@ -0,0 +1,11 @@
+from dataclasses import fields
+from django.forms import ModelForm
+from cms.models import PurchaseHistories
+
+
+class PurchaseItemForm(ModelForm):
+    ''' 購入したアイテムの登録・修正用のフォーム '''
+    class Meta:
+        model = PurchaseHistories
+        fields = ('payment_date', 'price',
+                  'major_category_id', 'payment_method_id', 'resident_id',)
