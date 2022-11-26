27bb21cfb46f779a327eb3cdcd28f29320891948
RyutoSuzuki <no.asics@gmail.com>
2022-11-26 04:36:31 +0000

    購入アイテムの登録、編集画面のパスを追加

--- cms/urls.py
+++ cms/urls.py
@@ -4,4 +4,6 @@ from cms import views
 app_name = 'cms'
 urlpatterns = [
     path('', views.purchase_histories_list, name='purchase_histories_list'),
+    path('purchase_item/add/', views.purchase_item_edit, name='purchase_item_add'),
+    path('purchase_item/edit/<int:purchase_item_id>', views.purchase_item_edit, name='purchase_item_edit'),
 ]
