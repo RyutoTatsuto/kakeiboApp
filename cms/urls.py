29f931eb25106cb19132ef147caa450d83755446
RyutoSuzuki <no.asics@gmail.com>
2022-12-19 14:54:16 +0000

    表記揺れの修正

--- cms/urls.py
+++ cms/urls.py
@@ -5,5 +5,6 @@ app_name = 'cms'
 urlpatterns = [
     path('', views.purchase_histories_list, name='purchase_histories_list'),
     path('purchase_item/add/', views.purchase_item_edit, name='purchase_item_add'),
-    path('purchase_item/edit/<int:purchase_item_id>', views.purchase_item_edit, name='purchase_item_edit'),
+    path('purchase_item/edit/<int:purchase_history_id>', views.purchase_item_edit, name='purchase_item_edit'),
+    path('purchase_item/delete/<int:purchase_history_id>', views.purchase_item_delete, name='purchase_item_delete'),
 ]
